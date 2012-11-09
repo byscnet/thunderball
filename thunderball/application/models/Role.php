a79626c063683e2484ea2ed6dd4ed14c3462f638
christiankeck <christian.keck@byscnet.de>
2012-11-09 15:27:49 +0100

    added gnu gpl notice in header

--- thunderball/application/models/Role.php
+++ thunderball/application/models/Role.php
@@ -1,4 +1,38 @@
 <?php
+/*
+ *   Copyright 2012 by byscnet.de - OpenSource Solutions
+ * 
+ * 	 This file is part of Thunderball Project Payment.
+ *
+ *   Thunderball Project Payment is free software: you can redistribute 
+ *   it and/or modify it under the terms of the GNU General Public License 
+ *   as published by the Free Software Foundation, either version 3 of the 
+ *   License, or (at your option) any later version.
+
+ *   Thunderball Project Payment is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Thunderball Project Payment.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Diese Datei ist Teil von Thunderball Project Payment.
+
+ *   Thunderball Project Payment ist Freie Software: Sie können es unter den Bedingungen
+ *   der GNU General Public License, wie von der Free Software Foundation,
+ *   Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren
+ *   veröffentlichten Version, weiterverbreiten und/oder modifizieren.
+
+ *   Thunderball Project Payment wird in der Hoffnung, dass es nützlich sein wird, aber
+ *   OHNE JEDE GEWÄHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ *   Gewährleistung der MARKTFÄHIGKEIT oder EIGNUNG FÜR EINEN BESTIMMTEN ZWECK.
+ *   Siehe die GNU General Public License für weitere Details.
+
+ *   Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ *   Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+
 /**
  * @Entity @Table(name="role")
  * Rolle
